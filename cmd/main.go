17b0637b7f22229540c340e0bed8d7e000de0db0
kekelberi <alexei.gaponov@gmail.com>
2022-07-03 21:31:51 +0300

    save

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+
 	"github.com/gaponovalexey/todo-app"
 	"github.com/gaponovalexey/todo-app/pkg/handler"
+	repository "github.com/gaponovalexey/todo-app/pkg/repo"
+	"github.com/gaponovalexey/todo-app/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handler := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("3000", handler.InitRoutes()); err != nil {
 		log.Fatalf("Error Server %s", err.Error())
