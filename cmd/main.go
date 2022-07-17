38ca0e69fb3a583c73f0257e286420a863f4be93
kekelberi <alexei.gaponov@gmail.com>
2022-07-17 17:20:56 +0300

    save

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	
+	"log"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", Midle(myF))
 
+	http.ListenAndServe(":3000", nil)
 }
 
+func myF(w http.ResponseWriter, r *http.Request) {
+	log.Println("you are fuck")
+	w.WriteHeader(http.StatusOK)
+}
+
+
+func Midle(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Panicln("Midle")
+		next(w, r)
+	}
+}
