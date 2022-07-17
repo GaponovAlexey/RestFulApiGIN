5b615f5bec0ccfa404a3a9a51eb0eaf1ff755b92
kekelberi <alexei.gaponov@gmail.com>
2022-07-17 16:05:34 +0300

    save

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gaponovalexey/todo-app"
-	"github.com/gaponovalexey/todo-app/pkg/handler"
-	"github.com/gaponovalexey/todo-app/pkg/repo"
-	"github.com/gaponovalexey/todo-app/pkg/service"
-	"github.com/spf13/viper"
+	
 )
 
 func main() {
-	if err := initConfig(); err != nil {
-		log.Fatalf("Error initial config: %s", err.Error())
-	}
 
-	repos := repository.NewRepository()
-	services := service.NewService(repos)
-	handler := handler.NewHandler(services)
-
-	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("3000"), handler.InitRoutes()); err != nil {
-		log.Fatalf("Error Server %s", err.Error())
-	}
 }
 
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
-}
