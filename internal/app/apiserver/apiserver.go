bc3e48dc27ce86eddef609f459efb197a1eebcec
kekelberi <alexei.gaponov@gmail.com>
2022-07-30 21:58:57 +0300

    save

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,16 +1,56 @@
 package apiserver
 
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
 //start
 func (s *APIServer) Start() error {
+	if err := s.configurateLoger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	s.logger.Info("starting api server")
+	return http.ListenAndServe(":3000", nil)
+}
+
+func (s *APIServer) configurateLoger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello")
+	}
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -2,12 +2,13 @@ package apiserver
 
 type Config struct {
 	BindAddr string `toml:"bind_addr`
+	LogLevel string `toml:"log_level`
 }
-
 
 // NewConfig...
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":3000",
+		LogLevel: "debug",
 	}
 }
