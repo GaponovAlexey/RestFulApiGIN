befd65208fac4ddf47eed3fa7cc2398c866c4dea
kekelberi <alexei.gaponov@gmail.com>
2022-08-01 13:17:58 +0300

    save

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 
-	"github.com/BurntSushi/toml"
 	"github.com/gaponovalexey/go-restapi/pkg/app/apiserver"
 )
 
-var (
-	configPath string
-)
-
-func init() {
-	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
-}
-
 func main() {
-	flag.Parse()
 
+	
 	config := apiserver.NewConfig()
-	_, err := toml.DecodeFile(configPath, &config)
-	fmt.Println("file",config)
-	
-	// fmt.Println(config)
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	s := apiserver.New(config)
+
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
--- pkg/app/apiserver/config.go
+++ pkg/app/apiserver/config.go
@@ -1,5 +1,11 @@
 package apiserver
 
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
 type Config struct {
 	BindAddr string `toml:"bind_addr`
 	LogLevel string `toml:"log_level`
@@ -11,4 +17,19 @@ func NewConfig() *Config {
 		BindAddr: ":3000",
 		LogLevel: "debug",
 	}
+}
+
+func init() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initial config: %s", err.Error()) // инициализация
+	}
+	var s Config
+	s.BindAddr = viper.GetString("config.BindAddr")
+	s.LogLevel = viper.GetString("config.LogLevel")
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs") // путь
+	viper.SetConfigName("config")  // файл
+	return viper.ReadInConfig()
 }
