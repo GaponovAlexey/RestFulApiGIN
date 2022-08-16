1294db8cbb727e9bc60c1c445486c5e01d54febe
kekelberi <alexei.gaponov@gmail.com>
2022-08-16 19:47:45 +0300

    save

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -15,8 +15,21 @@ func NewHandler(services *service.Service) *Handler {
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
-	router := gin.New()        // start gin
-	router.Use(cors.Default()) // safe cors
+	router := gin.New()
+	// start gin
+	router.Use(cors.New(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300, // Maximum value not ignored by any of major browsers
+
+		// Enable Debugging for testing, consider disabling in production
+		// Debug: true,
+	})) // safe cors
 
 	auth := router.Group("/auth")
 	{
