88d3a28b80aa19df590f5ea1a0519988db422632
kekelberi <alexei.gaponov@gmail.com>
2022-08-20 21:52:59 +0300

    save

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gaponovalexey/todo-app"
 	"github.com/gaponovalexey/todo-app/pkg/handler"
@@ -51,9 +54,26 @@ func main() {
 
 	//start
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
-		logrus.Fatalf("Error Server %s", err.Error())
+
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
+			logrus.Fatalf("Error Server %s", err.Error())
+		}
+	}()
+
+	logrus.Print("START todoApp")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	logrus.Print("todoApp STOP")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error srv:%s", err.Error())
 	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error db:%s", err.Error())
+	}
+
 }
 
 func initConfig() error {
